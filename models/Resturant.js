0eecbfbf3094e6aa050da0e7ca8eb74224ed9b15
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2023-03-01 12:46:42 +0100

    file:schema for resturant pos

---
+++ models/Resturant.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const restaurantModel = mongoose.Schema({
+  orderedItem: {
+    type: String,
+    required: true,
+  },
+  quantityOrdered: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 1000,
+  },
+});
+
+module.exports = restaurantModel;
