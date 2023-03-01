1896b0c039a2cd2fb6fe5d3d25db634f3f2889b9
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2023-03-01 11:42:42 +0100

    file:updated room model

--- models/RoomsModel.js
+++ models/RoomsModel.js
@@ -1,13 +1,3 @@
-// schema
-// 1 user - firstname,lastname,password, email, role,
-
-// roomsSchema
-//  { room - number,
-//  status,  utilities ,
-//  price, }
-
-// bookedRooms schema
-// number of booked rooms,checking out,number of booking days,cancelbooking
 const mongoose = require("mongoose");
 const roomSchema = mongoose.Schema({
   roomNumber: {
