1ab2ec6083d50d050e24470ad0b436d370068c46
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2023-03-04 00:00:10 +0100

    fix:fixed the controllers

--- routes/bookedRoomRoute.js
+++ routes/bookedRoomRoute.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const bookedRoomController = require("../controllers/BookedRoomController");
 router
   .route("/")
-  .get(bookedRoomController.getAllOrders)
-  .post(bookedRoomController.createAnOrder)
-  .patch(bookedRoomController.updateAnOrder)
-  .delete(bookedRoomController.cancelAnOrder);
+  .get(bookedRoomController.getAllBookings)
+  .post(bookedRoomController.bookAGuest)
+  .patch(bookedRoomController.updateBooking)
+  .delete(bookedRoomController.cancelABooking);
 
 module.exports = router;
+// bookAGuest, updateBooking, cancelABooking, getAllBookings;
